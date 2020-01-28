17bd00a61027bf1c4c9f52ffb093b4b760356ca7
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-01-28 13:23:03 -0500

    Arithmetic

--- src/main/java/com/lambton/Arithmetic.java
+++ src/main/java/com/lambton/Arithmetic.java
@@ -2,50 +2,62 @@ package com.lambton;
 
 public class Arithmetic {
     public int addition(int a, int b){
+        System.out.println("Sum of integers: "+(a+b));
         return a+b;
     }
 
     public float addition(float a, float b){
+        System.out.println("Sum of floats: "+(a+b));
         return a+b;
     }
 
     public String addition(String a, String b){
+        System.out.println("Sum of Strings: "+(a+" "+b));
         return a+b;
     }
 
     public String addition(String a, int b){
+        System.out.println("Sum of String and integer: "+(a+" "+b));
         return a+b;
     }
 
     public String addition(int a, String b){
+        System.out.println("Sum of integer and String: "+(a+" "+b));
         return a+b;
     }
 
     public int addition(int a, int b, int c){
-        return a+ addition(a,b);
+        System.out.println("Sum of integers: "+((a+b)+c));
+        return a+b+c;
     }
 
     public float addition(int a, int b, float c){
-        return (float)addition(a,b)+c;
+        System.out.println("Sum of integers and float: "+((a+b)+c));
+        return a+b+c;
     }
 
     public float addition(float a, int b){
-        return a+(float)b;
+        System.out.println("Sum of float and integer: "+(a+b));
+        return a+b;
     }
 
     public float addition(float a, int b, int c){
-        return a+(float)addition(a,b);
+        System.out.println("Sum of float and integers: "+(a+(b+c)));
+        return a+b+c;
     }
 
     public String addition(String a, int b, float c){
-        return a+(String)addition(a,b);
+        System.out.println("Sum of String, integer and float: "+a+" "+(b+c));
+        return a+b+c;
     }
 
     public double addition(int a, double b){
+        System.out.println("Sum of integer and double: "+(a+b));
         return a+b;
     }
 
     public float addition(int a, float b){
+        System.out.println("Sum of integer and float: "+(a+b));
         return a+b;
     }
 }
--- src/main/java/com/lambton/Main.java
+++ src/main/java/com/lambton/Main.java
@@ -4,9 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Arithmetic a=new Arithmetic();
         a.addition(2,3);
-        a.addition(5.0f,4);
-        a.addition(6,8.0f);
-        a.addition(7,4.0);
+        a.addition(5.0f,4.5f);
         a.addition("Nikita","Sandhu");
+        a.addition("Nikita",13);
+        a.addition(13,"Sandhu");
+        a.addition(14,15, 16);
+        a.addition(17,18, 19.5f);
+        a.addition(7.5f,4);
+        a.addition(19.5f,17, 18);
+        a.addition("Nikita",13, 25.3f);
+        a.addition(15,16.5);
+        a.addition(15,16.5f);
     }
 }
